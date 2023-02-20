756fb618084f4530dabe3320e1a90da377fb8b28
Pedro Henrique <phenriquerolim@gmail.com>
2023-02-20 12:31:27 -0300

    Adicionando Exception Handler

---
+++ src/main/java/com/pdrsoft/pdrfood/api/exceptionhandler/Problema.java
@@ -0,0 +1,16 @@
+package com.pdrsoft.pdrfood.api.exceptionhandler;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class Problema {
+
+    private LocalDateTime dataHora;
+    private String mensagem;
+
+}
